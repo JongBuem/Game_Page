92c3ad1662ebe29c80e70ada60aa0a030af79467
JongBuem <75786010+JongBuem@users.noreply.github.com>
2021-01-08 05:04:09 +0900

    게임종료 후 입력추가
    게임종료 후 정보를 alert 입력하여  localStorage에 이름과 종료시간을 저장

--- js/gameOver.js
+++ js/gameOver.js
@@ -2,17 +2,13 @@
 const pepoleOver = document.querySelector('.pepole');
 const blockOver = document.querySelector('.block');
 const block_2Over = document.querySelector('.block_2');
-const rankText = document.querySelector('.rank');
-const form = document.querySelector('.form');
-const nameInput = form.querySelector('input');
-let NAME = "clock";
 
 
 const blockOverBom = setInterval(()=>{
   const pepoleTop = parseInt(window.getComputedStyle(pepoleOver).getPropertyValue("top"));
   const blockLeft = parseInt(window.getComputedStyle(blockOver).getPropertyValue("left"));   
   if(blockLeft<70 && blockLeft>40 && pepoleTop==150 ){
-    gameOver(blockOver);
+    gameOver();
   }  
 },10);
 
@@ -20,53 +16,45 @@ const block_2OverBom = setInterval(()=>{
   const pepoleTop = parseInt(window.getComputedStyle(pepoleOver).getPropertyValue("top"));
   const blockLeft = parseInt(window.getComputedStyle(block_2Over).getPropertyValue("left"));
   if(blockLeft<70 && blockLeft>40 && pepoleTop==150 ){
-    gameOver(block_2Over);
+    gameOver();
   } else if(blockLeft<70 && blockLeft>40 && pepoleTop>150 && pepoleTop < 80){
-    gameOver(block_2Over);
+    gameOver();
   } 
 },10);
 
-function gameOver(block){
-    block.style.animation = 'none';
-    block.style.display ='none';
-    alert(clockTitle.innerText);
-    gameSave(clockTitle.innerText);
-    history.go(0);
+function gameOver(){
+  blockOver.style.animation = 'none';
+  blockOver.style.display ='none';
+  block_2Over.style.animation = 'none';
+  block_2Over.style.display ='none';
+  gameOverAlert();
+  history.go(0);
 }
 
-function save(clock){
+function gameOverAlert() {
+  let name;
+  const person = prompt(`clock : ${clock.innerText}\nPlease enter your name :`, "User");
+  if (person == null || person == ""  ? name = "User"  : name = person); 
+  gameSave(clock.innerText,name);
+}
+
+function gameNewSave(clock,NAME){
+  localStorage.setItem(NAME,clock);
+}
+
+function gameupdate(clock,NAME){
+  const save = localStorage.getItem(NAME);
+  if(save < clock){
     localStorage.setItem(NAME,clock);
-    rankText.innerHTML=clock;
+  }
 }
 
-function update(clock){
-    const savefile = localStorage.getItem(NAME,clock);
-    if(savefile < clock){
-        localStorage.setItem(NAME,clock);
-        rankText.innerHTML=clock;
-    }
-    else{
-        rankText.innerHTML=savefile;
-    }
+function gameSave(clock,NAME){
+  const save = localStorage.getItem(NAME);
+  if(save === null ? gameNewSave(clock,NAME)  : gameupdate(clock,NAME));
 }
 
-function gameSave(clock){
-   const savefile = localStorage.getItem(NAME);
-   if(savefile === null){
-       save(clock);
-   } else {
-       update(clock);
-   }
-}
 
-function inputt(event){
-    event.preventDefault(); 
-    const naem = nameInput.value;
-    nameInput.value = "";  
-    return naem;
-}                                       // 함수호출(1)
 
-function init(){                                               // 함수호출(1)
-    nameInput.addEventListener('submit', inputt);                     // 두번째 입력창에서 양식이 제출되면 함수호출(2)
-}
-init();
+
+
