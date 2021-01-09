e04e45a11c13fda27e4c85f11bf8b225bac753ed
JongBuem <75786010+JongBuem@users.noreply.github.com>
2021-01-10 05:08:13 +0900

    JS : 최종 키보드조작 변경
    기존의 피하는 방식이 아닌 높이값을 추가하여 캐릭터 위치를 바꾸게 함

--- js/keyboard.js
+++ js/keyboard.js
@@ -3,40 +3,41 @@ const pepole = document.querySelector('.pepole');
 const block = document.querySelector('.block');
 const block_2 = document.querySelector('.block_2');
 const Up = document.querySelector('.fa-caret-square-up');
-const Down = document.querySelector('.fa-caret-square-down')
+const Down = document.querySelector('.fa-caret-square-down');
+const pepoleTOP = document.querySelector('.pepole');
 
-function keyboardJump(){
-  if(pepole.classList !== 'jump'){                              // 점프를 하지 않을때
-    pepole.classList.add('jump');                               // 점프 모션 실행
-    Up.classList.add('up');                                     // 위 화살표 표시
-  }
-  setTimeout(() =>  pepole.classList.remove('jump'),700);       // 점프 이후 다시 점프 하기위해 삭제
-  setTimeout(() =>   Up.classList.remove('up'),700);            // 점프 이후 다시 본래의 색 변환
+function updowmImge(imge,className){
+  imge.classList.add(className);                                                        // 방향 입력시 red색을 주고                          
+  setTimeout(() => imge.classList.remove(className),700);                               // 다시 본래의 색을 준다
 }
 
-function keyboardSliding(){
-  if(pepole.classList !== 'sliding'){                           // 슬라이딩을 안할때
-    pepole.classList.add('sliding');                            // 슬라이딩 모션 가능
-    Down.classList.add('down');                                   // 아래화살표 표시
+function keyboardUp(){                                                                  
+  let pepole = parseInt(window.getComputedStyle(pepoleTOP).getPropertyValue("top"));    // 캐릭터의 높이 
+  if(pepole>-150){                                                                      // 게임화면 내에서만
+    pepoleTOP.style.top = pepole - 50+"px";                                             // 50씩 높이를 줄여 위로 올림
   }
-  setTimeout(() =>  pepole.classList.remove('sliding'),700);    // 슬라이딩 이후 다시 슬라이딩 하기위해 삭제
-  setTimeout(() =>   Down.classList.remove('down'),700);            // 슬라이딩 이후 다시 본래의 색 변환
+  updowmImge(Up,'up');                                                                  // 화살표 이미지
+}
+
+function keyboardDown(){
+  let pepole = parseInt(window.getComputedStyle(pepoleTOP).getPropertyValue("top"));    // 캐릭터의 높이 
+  if(pepole<0){                                                                         // 게임화면 내에서만
+    pepoleTOP.style.top = pepole + 50 +"px";                                            // 50씩 높이를 높여 아래로 내림
+  }                       
+  updowmImge(Down,'down');                                                              // 화살표 이미지
 }
 
 function keyboard() {         
-  if (window.event.keyCode == 38) {                             // 위 화살표 키코드
-    keyboardJump();
+  if (window.event.keyCode == 38) {                                                     // 위 화살표 키코드 확인
+    keyboardUp();
   } 
-  else if(window.event.keyCode == 40){                          // 아래화살표 키코드
-    keyboardSliding();
+  else if(window.event.keyCode == 40){                                                  // 아래 화살표 키코드 확인
+    keyboardDown();
   }
 }
 
-
 function init(){
-  window.addEventListener("keydown" ,keyboard);                 // 위 화살표 입력시 이벤트 호출
-  window.addEventListener("keydown" ,keyboard);                 // 아래화살표 입력시 이벤트 호출
+  window.addEventListener("keydown" ,keyboard);                                         // 위 화살표 입력시 이벤트 호출
+  window.addEventListener("keydown" ,keyboard);                                         // 아래 화살표 입력시 이벤트 호출
 }
 init();
-
-
